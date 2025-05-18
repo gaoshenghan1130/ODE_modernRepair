a248d87862996a9d37e9960be266015e2432067c
gsh <gsh1130@sjtu.edu.cn>
2025-05-18 14:47:44 +0800

    ignore更新，删去build，cmake版本限制更新

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/gaoshenghan/Desktop/ode
-# Build directory: /Users/gaoshenghan/Desktop/ode/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests "/Users/gaoshenghan/Desktop/ode/build/tests")
-set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/gaoshenghan/Desktop/ode/CMakeLists.txt;990;add_test;/Users/gaoshenghan/Desktop/ode/CMakeLists.txt;0;")
