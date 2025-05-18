178cd19560cb4802ba240ad19a9fc6c70308ddc8
gsh <gsh1130@sjtu.edu.cn>
2025-05-18 14:33:47 +0800

    cmake报错配置修复

---
+++ build/cmake_uninstall.cmake
@@ -0,0 +1,17 @@
+if(NOT EXISTS "/Users/gaoshenghan/Desktop/ode/build/install_manifest.txt")
+	message(FATAL_ERROR "Cannot find install manifest: \"/Users/gaoshenghan/Desktop/ode/build/install_manifest.txt\"")
+endif()
+
+file(READ "/Users/gaoshenghan/Desktop/ode/build/install_manifest.txt" FILES)
+string(REGEX REPLACE "\n" ";" FILES "${FILES}")
+
+foreach(FILE ${FILES})
+	message(STATUS "Uninstalling: ${FILE}")
+	if(EXISTS "${FILE}")
+		file(REMOVE ${FILE})
+	elseif(IS_SYMLINK "${FILE}")
+		file(REMOVE ${FILE})
+	else()
+		message(STATUS "File \"${FILE}\" does not exist.")
+	endif()
+endforeach()
